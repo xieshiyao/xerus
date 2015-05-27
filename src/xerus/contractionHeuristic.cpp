7117ab23e0d7f9bfbb13c821d5d185c55ffc3e03
Sebastian Wolf <git@haatschii.de>
2015-05-27 17:58:20 +0200

    Closes #67 removed the last useless function names

--- src/xerus/contractionHeuristic.cpp
+++ src/xerus/contractionHeuristic.cpp
@@ -90,9 +90,9 @@ namespace xerus {
         GREEDY(greedy_speed, (n*m-(n+m)*r)/(n*m*r))
         GREEDY(greedy_r, -r)
         
-        ContractionHeuristic::AddToVector g("greedy_size", greedy_size);
-        ContractionHeuristic::AddToVector s("greedy_speed", greedy_speed);
-        ContractionHeuristic::AddToVector r("greedy_r", greedy_r);
+        ContractionHeuristic::AddToVector greedy_size_heuristic("greedy_size", greedy_size);
+        ContractionHeuristic::AddToVector greedy_speed_heuristic("greedy_speed", greedy_speed);
+        ContractionHeuristic::AddToVector greedy_rank_heuristic("greedy_r", greedy_r);
     }
 
 }
