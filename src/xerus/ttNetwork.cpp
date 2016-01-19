5d0407ac4d3401e085a1ba9438f765ee071bcc54
Sebastian Wolf <git@haatschii.de>
2016-01-19 23:27:52 +0100

    Updated Copyright notice

--- src/xerus/ttNetwork.cpp
+++ src/xerus/ttNetwork.cpp
@@ -1,5 +1,5 @@
-// // // Xerus - A General Purpose Tensor Library
-// Copyright (C) 2014-2015 Benjamin Huber and Sebastian Wolf. 
+// Xerus - A General Purpose Tensor Library
+// Copyright (C) 2014-2016 Benjamin Huber and Sebastian Wolf. 
 // 
 // Xerus is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
