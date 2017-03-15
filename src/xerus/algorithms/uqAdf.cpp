635321fe11af733fb5fde5277df9f00b23007d9b
Sebastian Wolf <git@haatschii.de>
2017-03-15 18:47:47 +0100

    Fast ADF UQ working

--- src/xerus/algorithms/uqAdf.cpp
+++ src/xerus/algorithms/uqAdf.cpp
@@ -38,6 +38,8 @@ namespace xerus {
         
         const size_t N;
         const size_t d;
+		
+		const double solutionsNorm;
         
         const std::vector<std::vector<Tensor>> positions;
         const std::vector<Tensor>& solutions;
@@ -54,7 +56,6 @@ namespace xerus {
         
         
     public:
-        
         static Tensor randVar_to_position(const double _v, const size_t _polyDegree) {
             const std::vector<xerus::misc::Polynomial> stochasticBasis = xerus::misc::Polynomial::build_orthogonal_base(_polyDegree, [](const double){return 1.0;}, -1., 1.);
             
@@ -65,6 +66,7 @@ namespace xerus {
             
             return p;
         }
+        
         
         static std::vector<std::vector<Tensor>> create_positions(const TTTensor& _x, const std::vector<std::vector<double>>& _randomVariables) {
             std::vector<std::vector<Tensor>> positions(_x.degree());
@@ -79,10 +81,20 @@ namespace xerus {
             return positions;
         }
         
+        static double calc_solutions_norm(const std::vector<Tensor>& _solutions) {
+			double norm = 0;
+			for(const auto& s : _solutions) {
+				norm += misc::sqr(frob_norm(s));
+			}
+			
+			return std::sqrt(norm);
+		}
+        
         
         InternalSolver(TTTensor& _x, const std::vector<std::vector<double>>& _randomVariables, const std::vector<Tensor>& _solutions) : 
             N(_randomVariables.size()), 
             d(_x.degree()),
+            solutionsNorm(calc_solutions_norm(_solutions)),
             positions(create_positions(_x, _randomVariables)),
             solutions(_solutions),
             x(_x),
@@ -97,7 +109,6 @@ namespace xerus {
         
         
         void calc_left_stack(const size_t _corePosition) {
-//             LOG(bug, "Calc left stack @ " << _corePosition);
             REQUIRE(_corePosition+1 < d, "Invalid corePosition");
             
 			if(_corePosition == 0) {
@@ -105,7 +116,7 @@ namespace xerus {
 				shuffledX.reinterpret_dimensions({x.dimensions[0], x.rank(0)});
 				
 				for(size_t j = 0; j < N; ++j) {
-					contract(leftIsStack[_corePosition][j], shuffledX, true, shuffledX, false, 1); // TODO simple Identity!
+                    // NOTE: leftIsStack[0] is always an identity
 					contract(leftOughtStack[_corePosition][j], solutions[j], shuffledX, 1);
 				}
 				
@@ -116,36 +127,41 @@ namespace xerus {
 				for(size_t j = 0; j < N; ++j) {
 					contract(measCmp, positions[_corePosition][j], shuffledX, 1);
 					
+					if(_corePosition > 1) {
+						contract(tmp, measCmp, true, leftIsStack[_corePosition-1][j], false,  1);
+						contract(leftIsStack[_corePosition][j], tmp, false, measCmp, false, 1);
+					} else { // _corePosition == 1
+						contract(leftIsStack[_corePosition][j], measCmp, true, measCmp, false, 1);
+					}
 					
-					contract(tmp, measCmp, true, leftIsStack[_corePosition-1][j], false,  1);
-					contract(leftIsStack[_corePosition][j], tmp, false, measCmp, false, 1);
 					contract(leftOughtStack[_corePosition][j], leftOughtStack[_corePosition-1][j], measCmp, 1);
 					
 					
-					
-					if(_corePosition > 1) {
-						contract(leftStack[_corePosition][j], leftStack[_corePosition-1][j], measCmp, 1);
-					} else { // _corePosition == 1
-						leftStack[_corePosition][j] = measCmp;
-					}
+// 					if(_corePosition > 1) {
+// 						contract(leftStack[_corePosition][j], leftStack[_corePosition-1][j], measCmp, 1);
+// 					} else { // _corePosition == 1
+// 						leftStack[_corePosition][j] = measCmp;
+// 					}
 				}
 			}
 		}
 		
         
         void calc_right_stack(const size_t _corePosition) {
-//             LOG(bug, "Calc right stack @ " << _corePosition);
             REQUIRE(_corePosition > 0 && _corePosition < d, "Invalid corePosition");
-            const Tensor shuffledX = reshuffle(x.get_component(_corePosition), {1, 0, 2});
             
-            Tensor tmp;
-            for(size_t j = 0; j < N; ++j) {
-                contract(tmp, positions[_corePosition][j], shuffledX, 1);
-                
-                if(_corePosition < d-1) {
+            Tensor shuffledX = reshuffle(x.get_component(_corePosition), {1, 0, 2});
+            
+            if(_corePosition < d-1) {
+                Tensor tmp;
+                for(size_t j = 0; j < N; ++j) {
+                    contract(tmp, positions[_corePosition][j], shuffledX, 1);
                     contract(rightStack[_corePosition][j], tmp, rightStack[_corePosition+1][j], 1);
-                } else {
-                    contract(rightStack[_corePosition][j], tmp, one, 1);
+                }
+            } else { // _corePosition == d-1
+                shuffledX.reinterpret_dimensions({shuffledX.dimensions[0], shuffledX.dimensions[1]}); // Remove dangling 1-mode
+                for(size_t j = 0; j < N; ++j) {
+                    contract(rightStack[_corePosition][j], positions[_corePosition][j], shuffledX, 1);
                 }
             }
         }
@@ -195,60 +211,30 @@ namespace xerus {
         }
         
         
-        Tensor calculate_delta(const std::vector<Tensor>& _residual, const size_t _corePosition) {
-//             LOG(bug, "Calculate delta @ " << _corePosition);
+        Tensor calculate_delta(const size_t _corePosition) {
             //calculate_projected_gradient
 			Tensor delta(x.get_component(_corePosition).dimensions);
-			Tensor delta2(x.get_component(_corePosition).dimensions);
             Tensor dyadComp;
             
-// 			if(_corePosition > 0) {
-// 				for(size_t j = 0; j < N; ++j) {
-// 					// Calculate "direction" 
-// 					if(_corePosition < d-1) {
-// 						contract(dyadComp, positions[_corePosition][j], rightStack[_corePosition+1][j], 0);
-// 					} else {
-// 						dyadComp = positions[_corePosition][j];
-// 						dyadComp.reinterpret_dimensions({dyadComp.dimensions[0], 1});
-// 					}
-// 					
-// 					if(_corePosition > 1) {
-// 						contract(dyadComp, leftStack[_corePosition-1][j], dyadComp, 0);
-// 						contract(dyadComp, x.get_component(0), dyadComp, 1);
-// 					} else {
-// 						contract(dyadComp, x.get_component(0), dyadComp, 0);
-// 					}
-// 					
-// 					contract(dyadComp, one, dyadComp, 1); // TODO weg
-// 					
-// 					// Scale with residual
-// 					contract(dyadComp, _residual[j], dyadComp, 1);
-// 					
-// 					delta += dyadComp;
-// 				}
-// 			} else { // _corePosition == 0
-// 				for(size_t j = 0; j < N; ++j) {
-// 					contract(dyadComp, _residual[j], rightStack[_corePosition+1][j], 0);
-// 					
-// 					dyadComp.reinterpret_dimensions(delta.dimensions);
-// 					delta += dyadComp;
-// 				}
-// 			}
-            
-//             LOG(bug, "Calculate alternative delta @ " << _corePosition);
 			
 			Tensor tmp;
 			if(_corePosition > 0) {
 				const Tensor shuffledX = reshuffle(x.get_component(_corePosition), {1, 0, 2});
 				for(size_t j = 0; j < N; ++j) {
 					// Calculate "is"
-					contract(tmp, positions[_corePosition][j], shuffledX, 1);
-					contract(tmp, tmp, true, leftIsStack[_corePosition-1][j], false, 1);
-					if(_corePosition < d-1) {
-						contract(tmp, rightStack[_corePosition+1][j], true, tmp, false, 1);
-					} else {
-						contract(tmp, one, true, tmp, false, 1);
-					}
+                    contract(tmp, positions[_corePosition][j], shuffledX, 1);
+                    
+                    if(_corePosition < d-1) {
+                        contract(tmp, tmp, rightStack[_corePosition+1][j], 1);
+                    } else {
+                        tmp.reinterpret_dimensions({tmp.dimensions[0]});
+                    }
+                    
+                    
+                    if(_corePosition > 1) { // NOTE: For _corePosition == 1 leftIsStack is the identity
+                        contract(tmp, leftIsStack[_corePosition-1][j], tmp, 1);
+                    }
+                    
 					contract(tmp, tmp, positions[_corePosition][j], 0);
 					
 					if(_corePosition < d-1) {
@@ -258,7 +244,7 @@ namespace xerus {
 						dyadComp.reinterpret_dimensions({dyadComp.dimensions[0], dyadComp.dimensions[1], 1});
 					}
 					
-					delta2 += dyadComp;
+					delta += dyadComp;
 					
 					// Calculate "Ought"
 					contract(tmp, leftOughtStack[_corePosition-1][j], positions[_corePosition][j], 0);
@@ -270,7 +256,7 @@ namespace xerus {
 						dyadComp.reinterpret_dimensions({dyadComp.dimensions[0], dyadComp.dimensions[1], 1});
 					}
 					
-					delta2 -= dyadComp;
+					delta -= dyadComp;
 					
 				}
 			} else { // _corePosition == 0
@@ -281,56 +267,50 @@ namespace xerus {
 					
 					dyadComp.reinterpret_dimensions({1, x.dimensions[0]});
 					contract(dyadComp, dyadComp, rightStack[_corePosition+1][j], 0);
-					delta2 += dyadComp;
+                    delta += dyadComp;
 				}
 			}
             
-//             LOG(bug, "Difference: " << frob_norm(delta - delta2));
-            
-//             LOG(norm, "Norm @ " << _corePosition << ": " << frob_norm(delta2));
-            return delta2;
+            return delta;
         }
         
         
         double calculate_norm_A_projGrad(const Tensor& _delta, const size_t _corePosition) {
             double norm = 0.0;
-            
+			Tensor tmp;
+			
             if(_corePosition == 0) {
-                Tensor tmp;
-                
                 for(size_t j = 0; j < N; ++j) {
-                    
-                    // Spacial Part
                     contract(tmp, _delta, rightStack[1][j], 1);
-                    
                     norm += misc::sqr(frob_norm(tmp));
                 }
             } else { // _corePosition > 0
-                const Tensor shuffledX = reshuffle(_delta, {1, 0, 2});
-                Tensor tmp;
+                Tensor shuffledX = reshuffle(_delta, {1, 0, 2});
+				if(_corePosition == d-1) {
+					shuffledX.reinterpret_dimensions({shuffledX.dimensions[0], shuffledX.dimensions[1]}); // Remove dangling 1-mode
+				}
                 
-                for(size_t j = 0; j < N; ++j) {
-                    
-                    // Current node
-                    contract(tmp, positions[_corePosition][j], shuffledX, 1);
-                    
-                    // Right part
-                    if(_corePosition < d-1) {
-                        contract(tmp, tmp, rightStack[_corePosition+1][j], 1);
-                    } else {
-                        contract(tmp, tmp, one, 1);
-                    }
-                    
-                    // Left Part
-                    if(_corePosition > 1) {
-                        contract(tmp, leftStack[_corePosition-1][j], tmp, 1);
-                    }
-                    
-                    // Spacial Part
-                    contract(tmp, x.get_component(0), tmp, 1);
-                    
-                    norm += misc::sqr(frob_norm(tmp));
-                }
+				Tensor rightPart;
+				for(size_t j = 0; j < N; ++j) {
+					// Current node
+					contract(tmp, positions[_corePosition][j], shuffledX, 1);
+					
+					if(_corePosition < d-1) {
+						contract(rightPart, tmp, rightStack[_corePosition+1][j], 1);
+					} else {
+						rightPart = tmp;
+					}
+					
+					if(_corePosition > 1) {
+						contract(tmp, rightPart, leftIsStack[_corePosition-1][j], 1);
+						contract(tmp, tmp, rightPart, 1);
+					} else { // NOTE: For _corePosition == 1 leftIsStack is the identity
+						contract(tmp, rightPart, rightPart, 1);
+					}
+					
+					REQUIRE(tmp.size == 1, "IE");
+					norm += tmp[0];
+				}
             }
             
             return std::sqrt(norm);
@@ -352,14 +332,26 @@ namespace xerus {
             return std::sqrt(norm);
         }
         
-        double calc_solutions_norm() {
-            double norm = 0;
-            for(const auto& s : solutions) {
-                norm += misc::sqr(frob_norm(s));
-            }
-            
-            return std::sqrt(norm);
-        }
+        
+        
+        
+        
+        
+        double calc_residual_norm(const size_t _corePosition) {
+			REQUIRE(_corePosition == 0, "Invalid corePosition");
+
+			double norm = 0.0;
+			
+			Tensor tmp;
+			for(size_t j = 0; j < N; ++j) {
+				contract(tmp, x.get_component(0), rightStack[1][j], 1);
+				tmp.reinterpret_dimensions({x.dimensions[0]});
+				tmp -= solutions[j];
+				norm += misc::sqr(frob_norm(tmp));
+			}
+			
+			return std::sqrt(norm);
+		}
         
         void solve() {
 			
@@ -381,13 +373,14 @@ namespace xerus {
 				
 				for(size_t corePosition = 0; corePosition < x.degree(); ++corePosition) {
 					
-					const auto residual = calc_residual(corePosition);
+// 					const auto residual = calc_residual(corePosition);
 					
 					if(corePosition == 0) {
-						LOG(bla, "Residual norm: " << residual_norm(residual)/calc_solutions_norm());
+// 						LOG(bla, "Residual @ " << corePosition << " norm: " << std::scientific << residual_norm(residual)/solutionsNorm);
+						LOG(bla, "Residual norm: " << std::scientific << calc_residual_norm(0)/solutionsNorm);
 					}
 					
-					const auto delta = calculate_delta(residual, corePosition);
+					const auto delta = calculate_delta(corePosition);
 					
 					const auto normAProjGrad = calculate_norm_A_projGrad(delta, corePosition);
 					
